7ca4684a71020f0cf90e006218f27a243dfc9279
Jorge Agundez Medina <jamedina97@gmail.com>
2015-03-09 21:35:05 +0000

    adding login markup for home page

--- secret_santa/index.php
+++ secret_santa/index.php
@@ -17,14 +17,33 @@ $template = 'frontpage';
 									<span class="icon-bar"></span>
 									<span class="icon-bar"></span>
 								</button>
-								<a class="navbar-brand" href="#">Amigo Invisible</a>
+								<a class="navbar-brand" href="#">Secret Santa</a>
 							</div>
 							<!-- Collect the nav links, forms, and other content for toggling -->
 							<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-								<ul class="nav navbar-nav navbar-right">
-									<li><a href="#">Home</a></li>
-									<li><a href="#">About</a></li>
-								</ul>
+								<form role="form" id="login_form" method="post" action="controller/login.php">
+									<ul class="nav navbar-nav navbar-right">
+										<li><a href="#">Home</a></li>
+										<li><a href="#">About</a></li>
+										<li>
+											<a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">Login</a>
+											<ul class="collapse" id="collapseExample">
+												<li>
+													<label for="username">User</label>
+													<input type="text" name="username" class="form-control" id="username" placeholder="Write here your username" required="true"/>
+												</li>
+									            <li>
+									            	<label for="password">Password</label>
+									            	<input type="password" name="password" class="form-control" id="password" placeholder="Write here your password" required="true"/></li>
+									            <li class="divider"></li>
+									            <li>
+									            	<input type="hidden" name="form_token" value="<?php echo $form_token; ?>" />
+									            	<button type="submit" class="btn btn-default">Login</button>
+									            </li>
+											</ul>	
+										</li>
+									</ul>
+								</form>
 							</div><!-- /.navbar-collapse -->
 						</div><!-- /.container-fluid -->
 					</nav>
@@ -32,39 +51,12 @@ $template = 'frontpage';
 			</div><!--/.fullwidth_wraper-->
 		</header>
 
-		<section class="banner">
-			<div class="container">
-				<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-					<!-- Indicators -->
-					<ol class="carousel-indicators">
-					<!-- 	<li data-target="#carousel-example-generic" data-slide-to="0"></li> -->
-						<li data-target="#carousel-example-generic" data-slide-to="1"></li>
-					</ol>
-
-					<!-- Wrapper for slides -->
-					<div class="carousel-inner" role="listbox">
-						<div class="item active">
-							<img src="images/prebanner.jpg" alt="..." class="img-responsive banner_image">
-							<div class="carousel-caption">
-							<p>Home</p>
-							</div>
-						</div>
-						<div class="item">
-							<img src="images/prebanner.jpg" alt="..." class="img-responsive banner_image">
-							<div class="carousel-caption">
-							<p>Log in</p>
-							</div>
-						</div>
-				</div>
-				<!-- Controls -->
-				<!-- <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-					<span class="glyphicon glyphicon-chevron-left"></span>
-					<span class="sr-only">Previous</span>
-				</a>
-				<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-					<span class="glyphicon glyphicon-chevron-right"></span>
-					<span class="sr-only">Next</span>
-				</a> -->
+		<section class="container">
+			<div class="banner">
+				<div class="row">
+					<div class="col-md-12">
+						<img src="/secret_santa/images/prebanner1.jpg" alt="banner" />
+					</div>
 				</div>
 			</div> <!-- container -->
 		</section>
@@ -90,13 +82,7 @@ $template = 'frontpage';
 				</div>
 				<div class="row">
 					<div class="col-md-6 col-md-offset-3 well">
-						<button type="button" class="btn btn-success start_button">Comenzar</button>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-4 col-md-offset-4 well">
-						<h4>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo</h4>
-						<button type="button" class="btn btn-default  start_button">Log in</button>
+						<button type="button" class="btn btn-success start_button">Get Start</button>
 					</div>
 				</div>
 			</div>
