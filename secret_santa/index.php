7f51b1496f371fe3272d8edeb664de5b9d861215
Jorge Agundez Medina <jamedina97@gmail.com>
2015-05-27 22:34:30 +0100

    adding step one page style

--- secret_santa/index.php
+++ secret_santa/index.php
@@ -11,52 +11,64 @@ $template = 'frontpage';
 			
 	<body class="<?php echo $template ?>">
 
-		<header>
-			<div class="fullwidth_wraper">
+		<header class="yellow">
+
+			<div class="fullwidth_wraper white">
 				<div class="container">
-					<nav class="main_nav" role="navigation">
-						<a class="navbar-brand" href="#">Secret <span>Santa</span></a>
-						<a class="navbar-brand pull-right" href="#"><small>About</small></a>
-					</nav>
+					<div class="row">
+						<div class="col-xs-12">
+							<nav class="main_nav" role="navigation">
+								<a class="top_bar" href="#">Secret <span class="red">Santa</span></a>
+								<a class="top_bar pull-right" href="#"><small>About</small></a>
+							</nav>
+						</div>
+					</div>	
 				</div><!--/.container-->
 			</div><!--/.fullwidth_wraper-->
 		
-			<section class="container">
-				<div class="row">
-					<div class="col-xs-12">
-						<div class="logo">
-							<h1>Organiza tu propio amigo invisible en <br/><span class="red">60</span><span class="white">segundos!</span></h1>
+			<section class="logo_wrap">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="logo">
+								<h1><span class="blue">Organiza tu propio amigo invisible en</span><br/><span class="red">60</span><span class="white">segundos!</span></h1>	
+							</div>
 						</div>
 					</div>
 				</div>
-				<div class="row">
-					<div class="col-xs-10 col-xs-offset-1">
-						<form role="form" id="login_form" method="post" action="controller/login.php" class="login">
-							<div class="login_header">
-								<h3>Login</h3>
-							</div>
-							<div class="row">
-								<div class="col-xs-10 col-xs-offset-1 input_wrapper">
-									<label for="username">Usuario</label>
-									<input type="text" name="username" class="form-control" id="username" placeholder="username" required="true"/>
+			</section>
+
+			<section class="login_wrap">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-10 col-xs-offset-1">
+							<form role="form" id="login_form" method="post" action="controller/login.php" class="login">
+								<div class="login_header">
+									<h3>Login</h3>
 								</div>
-							</div>
-							<div class="row">
-								<div class="col-xs-10 col-xs-offset-1 input_wrapper">
-					            	<label for="password">Password</label>
-					            	<input type="password" name="password" class="form-control" id="password" placeholder="password" required="true"/></li>
-			         			</div>
-			         		</div>
-			         		<div class="row">
-			         			<div class="col-xs-10 col-xs-offset-1 input_wrapper">
-					            	<input type="hidden" name="form_token" value="<?php echo $form_token; ?>" />
-					            	<button type="submit" class="btn btn-red">Go!</button>
-					            </div>
-							</div>
-						</form>
+								<div class="row">
+									<div class="col-xs-10 col-xs-offset-1 input_wrapper">
+										<label for="username">Usuario</label>
+										<input type="text" name="username" class="form-control" id="username" placeholder="username" required="true"/>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-xs-10 col-xs-offset-1 input_wrapper">
+						            	<label for="password">Password</label>
+						            	<input type="password" name="password" class="form-control" id="password" placeholder="password" required="true"/></li>
+				         			</div>
+				         		</div>
+				         		<div class="row">
+				         			<div class="col-xs-10 col-xs-offset-1 input_wrapper">
+						            	<input type="hidden" name="form_token" value="<?php echo $form_token; ?>" />
+						            	<button type="submit" class="btn btn-red">Go!</button>
+						            </div>
+								</div>
+							</form>
+				        </div>
 			        </div>
-		        </div>
-			</section><!-- /container -->
+			    <div><!-- /container -->
+			</section>
 		</header>
 
 		<div class="">
@@ -79,7 +91,7 @@ $template = 'frontpage';
 					<div class="col-xs-12">
 						<div class="icon"></div>
 						<h2>¿Cómo funciona?</h2>
-						<p>Es muy sencillo, tan sólo tienes que seguir los tres siguientes sencillos pasos y tendrás acceso a tu panel de control para hacer un seguimiento y actualizar el juego siempre que quieras</p>
+						<p>Simple, tan sólo tienes que seguir los tres siguientes sencillos pasos y tendrás acceso a tu propio panel de control</p>
 					</div>
 				</div>
 			</div>
@@ -90,7 +102,7 @@ $template = 'frontpage';
 				<div class="row">
 					<div class="col-xs-10 col-xs-offset-1">
 						<div class="step_wrap">
-							<div class="step step_one"></div>
+							<div class="step pluma"></div>
 							<h2>Paso 1</h2>
 							<p class="bold">Rellena tus datos</p>
 							<p>Tan sólo usando tu dirección de email, elige un usuario y contraseña para comenzar</p>
@@ -98,7 +110,7 @@ $template = 'frontpage';
 					</div>
 					<div class="col-xs-10 col-xs-offset-1">
 						<div class="step_wrap">
-							<div class="step step_two"></div>
+							<div class="step puzzle"></div>
 							<h2>Paso 2</h2>
 							<p class="bold">Reglas del Juego</p>
 							<p>Establece las normas de tu grupo, temática, margen de precio, lugar de encuentro y todo lo que quieras!</p>
@@ -106,7 +118,7 @@ $template = 'frontpage';
 					</div>
 					<div class="col-xs-10 col-xs-offset-1">
 						<div class="step_wrap">
-							<div class="step step_three"></div>
+							<div class="step ninos"></div>
 							<h2>Paso 3</h2>
 							<p class="bold">Invita a tus amigos</p>
 							<p>Hazte con el correo electrónico de tus amigos y tan pronto todos acepten su invitación el sorteo de nombres llegará por email</p>
@@ -115,7 +127,7 @@ $template = 'frontpage';
 				</div>
 				<div class="row">
 					<div class="col-xs-6 col-xs-offset-3">
-						<a href="/secret_santa/stepOne.php" type="button" class="btn btn-success btn-red">Get Start</a>
+						<a href="/secret_santa/stepOne.php" type="button" class="btn btn-success btn-red">Comenzar</a>
 					</div>
 				</div>
 			</div>
--- secret_santa/stepOne.php
+++ secret_santa/stepOne.php
@@ -8,67 +8,84 @@ $_SESSION['form_token'] = $form_token;
 include "includes/header.php";
 ?>
 
-  <body>
-    <div class="container">
-      <header>
-        <h1>STEP 1: Subscription</h1>
-      </header>
-    </div>
-    <div class="container">
-      <form role="form" id="stepOne" method="post" action="controller/stepOne.php">
-        <div class="col-md-5 well">
-          <div class="row">
-            <div class="col-md-12">
-              <?php if (isset($_SESSION['error'])){ echo $_SESSION['error']; unset($_SESSION['error']);}?>
+<body>
+    <header>
+        <div class="fullwidth_wraper white">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <p class="top_bar"><span class="red">Paso 1 <span class="gray">|</span></span> Rellena tus Datos</p>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div class="row">
-            <div class="col-md-12">
-              <label for="username">User</label>
-              <input type="text" name="username" class="form-control" id="username" placeholder="Choose an user name" required="true"/>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-12">
-              <label for="email">Email</label>
-              <input type="email" name="email" class="form-control" id="email" placeholder="Write here your email" required="true"/>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-12">
-              <label for="rEmail">Repeat Email</label>
-              <input type="email" name="rEmail" class="form-control" id="rEmail" placeholder="Write here your email" required="true"/>
-            </div>
-          </div>
-          <div class="row">
-             <div class="col-md-12">
-              <label for="password">Password</label>
-              <input type="password" name="password" class="form-control" id="password" placeholder="Password" required="true"/>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-12">
-              <label for="rPassword">Repeat Password</label>
-              <input type="password" name="rPassword" class="form-control" id="rPassword" placeholder="Password" required="true"/>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-2">
-              <br>
-              <input type="hidden" name="form_token" value="<?php echo $form_token; ?>" />
-              <button type="submit" class="btn btn-default">Next Step</button>
-            </div>
-          </div>
         </div>
-      </form>
-    </div>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12 well">
-          <a href="/secret_santa/index.php" class="btn btn-default" >Do you have an account already? Login here</a>
+    </header>
+
+    <section class="form_step_wrapper blue steps">
+        <div class="step_wrap">
+            <div class="step ciervo"></div>
+            <div class="container">
+                <form role="form" id="stepOne" method="post" action="controller/stepOne.php">
+                    <div class="col-xs-12">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <?php if (isset($_SESSION['error'])){ echo $_SESSION['error']; unset($_SESSION['error']);}?>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12 input_wrapper">
+                                <label for="username">Usuario</label>
+                                <input type="text" name="username" class="form-control" id="username" placeholder="Elige un nombre de usuario" required="true"/>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12 input_wrapper">
+                                <label for="email">Email</label>
+                                <input type="email" name="email" class="form-control" id="email" placeholder="Escribe tu email" required="true"/>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12 input_wrapper">
+                                <label for="rEmail">Repite Email</label>
+                                <input type="email" name="rEmail" class="form-control" id="rEmail" placeholder="Repite tu email" required="true"/>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12 input_wrapper">
+                                <label for="password">Password</label>
+                                <input type="password" name="password" class="form-control" id="password" placeholder="Password" required="true"/>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12 input_wrapper">
+                                <label for="rPassword">Repite Password</label>
+                                <input type="password" name="rPassword" class="form-control" id="rPassword" placeholder="Password" required="true"/>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-2">
+                                <br>
+                                <input type="hidden" name="form_token" value="<?php echo $form_token; ?>" />
+                                <button type="submit" class="btn btn-red">Siguiente Paso</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="go_back">
+                <p>*Tienes una cuenta?</p>
+                <p>Haz login <a href="/secret_santa/index.php" class="" ><span class="gray">Aquí</span></a></p>
+            </div>
         </div>
-      </div>
-    </div>
+    </section>
+
+    <section class="container">
+        <div class="row">
+            <div class="col-md-12">
+                
+            </div>
+        </div>
+    </section>
 
 <?php 
 include "includes/footer.php";
